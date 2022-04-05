6110b680ca76997976f7d860301f6402f3eb376e
Patricio Vargas <pato@onesignal.com>
2022-04-05 11:35:11 -0400

    OneSignal Expo IAM setup

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+import Constants from "expo-constants";
 
 export default function App() {
+  OneSignal.setAppId("f8cc348c-2eb8-44c9-9905-5f1dd35ad547");
+  
+  //This is what will trigger the In-App Message when the component loads
+  OneSignal.addTrigger("level", 5);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
